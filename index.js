9c8b1c2edb889801e75af8ea9a42d2085fa14fe4
nathaniel roush <nathan.w.roush@gmail.com>
2023-04-17 16:42:05 -0400

    my arithmetic lab

---
+++ index.js
@@ -0,0 +1,47 @@
+function add() {
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    return "something";
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return "something";
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return "something";
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return "something";
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return n += 1;
+}
+function decrement(n) {
+    return n -+ 1;
+}
+function makeInt(n) {
+    return parseInt (n, 10)
+}
+ function preserveDecimal(n) {
+    return parseFloat (n, 10.88)
+ }
